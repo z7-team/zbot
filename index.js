ec8edfa2ea493dcbcb1618b0086d270026b7e379
Danny Rizko <drizko07@gmail.com>
2020-10-29 23:53:11 -0700

    Add rank and karma commands

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const admin = require('firebase-admin');
 const { prefix, token } = require('./config.json');
+const serviceAccount = require('./z7-bot-db-auth.json');
 
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 
-client.on('message', message =>{
+client.on('message', async (message) => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).trim().split(' ');
 	const command = args.shift().toLowerCase();
@@ -32,6 +40,56 @@ client.on('message', message =>{
 			msg += ('[ ' + (Math.floor(Math.random() * 6) + 1) + ' ] ');
 		}
 		message.channel.send(msg);
+	}
+	else if (command === 'rank') {
+		const usersRef = db.collection(message.guild.name);
+		const users = await usersRef.orderBy('karma', 'desc').get();
+
+
+		let leaderboard = '';
+		let place = 1;
+		await users.docs.forEach(async user => {
+			const userData = user.data();
+			message.guild.members.fetch(userData.id).then(guildMember => {
+				leaderboard += `${place}. **${guildMember.user.username} :** ${userData.karma}\n`;
+				place += 1;
+			});
+		});
+
+		const embed = new Discord.MessageEmbed()
+			.setTitle('Rank Leaderboard')
+			.setDescription(leaderboard);
+
+
+		await message.channel.send(
+			embed,
+		);
+	}
+	else if (args[0] == '++' || args[0] == '--') {
+		const mention = message.mentions.users.first();
+		if (message.author.id == mention.id) {
+			message.channel.send('Can\'t give karma to yourself');
+		}
+		const userRef = db.collection(message.guild.name).doc(mention.id);
+		const snapshot = await userRef.get();
+		let karma = snapshot.data()['karma'];
+
+		if (karma === undefined) {
+			karma = 0;
+		}
+		else if (args[0] === '++') {
+			karma++;
+		}
+		else if (args[0] === '--') {
+			karma--;
+		}
+		await userRef.set({
+			karma,
+			id: mention.id.toString(),
+		});
+
+		const pointStr = karma === 1 ? 'point' : 'points';
+		await message.channel.send(`${mention.username} you now have ${karma} ${pointStr}`);
 	}
 	else {
 		message.channel.send('Not a command. Type zz help for commands.');
