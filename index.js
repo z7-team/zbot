5d0aad4fcf1395b0a191b22e139adff47e8256a0
Mark Rizko <mark@rizkocircle.com>
2020-10-30 00:06:31 -0700

    starting reddit stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
+var snoowrap = require('snoowrap');
 
 
 client.once('ready', () => {
